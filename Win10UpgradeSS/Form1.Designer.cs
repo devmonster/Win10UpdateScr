f9fc9f4ba9533d6ea624f3aa1c59a8b80f6203cd
DW M <DW M@RAN-PC>
2017-09-14 14:54:50 +0800

    Add project files.

---
+++ Win10UpgradeSS/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Win10UpgradeSS
+{
+    partial class ScreenSaverMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenSaverMain));
+            this.uxCenterPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.uxCenterPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uxCenterPanel
+            // 
+            this.uxCenterPanel.Controls.Add(this.label3);
+            this.uxCenterPanel.Controls.Add(this.label2);
+            this.uxCenterPanel.Controls.Add(this.label1);
+            this.uxCenterPanel.Controls.Add(this.pictureBox1);
+            this.uxCenterPanel.ForeColor = System.Drawing.Color.White;
+            this.uxCenterPanel.Location = new System.Drawing.Point(359, 447);
+            this.uxCenterPanel.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.uxCenterPanel.Name = "uxCenterPanel";
+            this.uxCenterPanel.Size = new System.Drawing.Size(638, 185);
+            this.uxCenterPanel.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(256, 77);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "15% complete";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(177, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(340, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Configuring Windows Updates";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Do not turn off your computer.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(106, 62);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ScreenSaverMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1463, 976);
+            this.Controls.Add(this.uxCenterPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "ScreenSaverMain";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "uxMain";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.ScreenSaverMain_Load);
+            this.Shown += new System.EventHandler(this.ScreenSaverMain_Shown);
+            this.DoubleClick += new System.EventHandler(this.ScreenSaverMain_DoubleClick);
+            this.uxCenterPanel.ResumeLayout(false);
+            this.uxCenterPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel uxCenterPanel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Win10UpgradeSS/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win10UpgradeSS
+{
+    public partial class ScreenSaverMain : Form
+    {
+        public ScreenSaverMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ScreenSaverMain_DoubleClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreenSaverMain_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void ScreenSaverMain_Shown(object sender, EventArgs e)
+        {
+              var screenBounds = Screen.FromControl(this).Bounds;
+
+            this.Width = screenBounds.Width;
+            this.Height = screenBounds.Height;
+            this.Top = 0;
+            this.Left = 0;
+
+            uxCenterPanel.Top = this.Height / 2 - (uxCenterPanel.Height / 2);
+            uxCenterPanel.Left = this.Width / 2 - (uxCenterPanel.Width / 2);
+        }
+    }
+}
